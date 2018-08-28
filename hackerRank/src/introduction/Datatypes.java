29a4252b1978a751261fe458aee18d23b1a6b3a8
Kelvin <kelvin.costa@cognizant.com>
2018-08-27 21:10:08 -0300

    master - adding Datatypes and EndOfFile pratice chanllenges

---
+++ hackerRank/src/introduction/Datatypes.java
@@ -0,0 +1,43 @@
+package introduction;
+
+import java.util.*;
+
+class Datatypes {
+
+    public static void main(String[] argh) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+
+            try {
+                int count = 0;
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if (x >= -128 && x <= 127) {
+                    count++;
+                    System.out.println("* byte");
+                }
+                if (x >= -32768 && x <= 32767) {
+                    count++;
+                    System.out.println("* short");
+                }
+                if (x >= -(Math.pow(2, 31)) && x <= Math.pow(2, 31) - 1) {
+                    count++;
+                    System.out.println("* int");
+                }
+                if (x >= -(Math.pow(2, 63)) && x <= Math.pow(2, 63) - 1) {
+                    count++;
+                    System.out.println("* long");
+                }
+                if (count == 0) {
+                    System.out.println(x + " can't be fitted anywhere.");
+                }
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+
+        }
+    }
+}
---
+++ hackerRank/src/introduction/EndOfFile.java
@@ -0,0 +1,16 @@
+package introduction;
+
+import java.util.Scanner;
+
+public class EndOfFile {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        while(sc.hasNext()){
+            count++;
+            String str = sc.nextLine();
+            System.out.println(count + " " + str);
+        }
+    }
+}
